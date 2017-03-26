d20b6120b7c77a65b4ed1e9eafb31d919c08593a
Michael Chadwick <mechadwick@gmail.com>
2017-03-25 23:43:18 -0400

    Adding Scripts

---
+++ drive/Sync-All-The-Things-Verbose.sh
@@ -0,0 +1,30 @@
+echo "Syncing Google Drive Folders!"
+
+cd /media/Google-Drive
+
+echo "Syncing Aviation"
+drive pull Aviation
+
+echo "Syncing Companies"
+drive pull Companies
+
+echo "Syncing Current_Working"
+drive pull Current_Working
+
+echo "Syncing Games"
+drive pull Games
+
+echo "Syncing Photo_Backup"
+drive pull Photo_Backup
+
+echo "Syncing Records"
+drive pull Records
+
+echo "Syncing Sarah Documents"
+drive pull "Sarah Documents"
+
+echo "Syncing SchoolWork"
+drive pull SchoolWork
+
+echo "Syncing Tools"
+drive pull Tools
